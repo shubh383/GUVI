e209e502133376cc5f592e6ad99e2ce19cc7e9ac
shubh383 <41055660+shubh383@users.noreply.github.com>
2019-05-27 19:03:22 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a==0)
+	{
+		printf("Zero");
+	}
+	else if(a>0)
+	{
+		printf("Positive");
+	}
+	else
+	{
+		printf("negative");
+	}
+	return 0;
+}
