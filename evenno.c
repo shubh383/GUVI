bd6bf2bffe89ad887e891097033d0aab9f982c14
SHUBHAM KUMAR <shubhamkumarbidupur@gmail.com>
2019-07-10 13:20:51 +0530

    Create evenno.c

---
+++ evenno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int n,p,q,r;
+   scanf("%d %d",&p,&q);
+   for(r=p+1;r<q;++r)
+   {
+       if(r%2==0)
+       {
+           printf("%d ",r);
+       }
+   }
+  return 0;
+}
