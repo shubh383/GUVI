27ed63e99462f4543af101d86ed2ce81f5f42e22
SHUBHAM KUMAR <shubhamkumarbidupur@gmail.com>
2019-07-10 14:16:19 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,sum=0,r,temp,c;
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		r=n%10;
+		c=r*r*r;
+		sum=sum+c;
+		n=n/10;
+		
+	}
+	n=temp;
+	if(n==sum)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+}
