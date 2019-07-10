7312e36da178f79dac0f67d5dbea039fda0cff2f
SHUBHAM KUMAR <shubhamkumarbidupur@gmail.com>
2019-07-10 13:09:55 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,k=0;
+	scanf("%d",&n);
+	for(i=2;i<=n/2;++i)
+	{
+	    if(n%i==0)
+	    {
+	        k=1;
+	        break;
+	    }
+	}
+	if(k==0)
+	{
+	    printf("yes");
+	}
+	else
+	{
+	    printf("no");
+	}
+return 0;
+}
